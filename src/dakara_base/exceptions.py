b61c98741117707a0aac34dd987bb3d8ba5b9a2c
Neraste <neraste.herr10@gmail.com>
2022-02-02 01:30:51 +0900

    Add all exceptions handler

--- src/dakara_base/exceptions.py
+++ src/dakara_base/exceptions.py
@@ -7,40 +7,12 @@ library. All exception classes should inherit from `DakaraError`:
 ...     pass
 
 This helps to differentiate known exceptions and unknown ones, which are real
-bugs. On program execution, a try/except structure determines the reason of
-interruption of the program:
+bugs.
 
->>> import logging
->>> debug = False
->>> try:
-...     # your program here
-... except KeyboardInterrupt:
-...     logging.info("Quit by user")
-...     exit(255)
-... except SystemExit:
-...     logging.info("Quit by system")
-...     exit(254)
-... except DakaraError as error:
-...     # known error
-...     if debug:
-...         # directly re-raise the error in debug mode
-...         raise
-...     # just log it otherwise
-...     logging.critical(error)
-...     exit(1)
-... except BaseException as error:
-...     # unknown error
-...     if debug:
-...         # directly re-raise the error in debug mode
-...         raise
-...     # re-raise it and show a special message otherwise
-...     logging.exception("Unexpected error: {}".format(error))
-...     logging.critical("Please fill a bug report at <url of bugtracker>")
-...     exit(128)
->>> exit(0)
-
-It also devifes `generate_exception_handler` that allows to generate functions which
-will catch an exception, add a custom error message and re-raise it.
+It defines `generate_exception_handler` that allows to generate functions which
+will catch an exception, add a custom error message and re-raise it. It also
+defines `handle_all_exceptions` that can be used on `__main__` module to catch
+all program executions.
 """
 
 from contextlib import contextmanager
@@ -83,3 +55,55 @@ def generate_exception_handler(exception_class, error_message):
             raise error.__class__("{}\n{}".format(error, error_message)) from error
 
     return function
+
+
+@contextmanager
+def handle_all_exceptions(logger, bugtracker_url, debug=False):
+    """Handle all exceptions and yield a return value.
+
+    >>> with handle_all_exceptions(
+    ...    logging,
+    ...    "https://www.example.com/bugtracker"
+    ... ) as exit_value:
+    ...    # your program here
+    >>> exit(exit_value["value"])
+
+    Args:
+        logger (logging.Logger): Logger.
+        bugtracker_url (str): URL address of the bugtracker, displayed on
+            unexpected exceptions.
+        debug (bool): If True, known and unknown exceptions will be directly
+            raised.
+
+    Yield:
+        dict: Container with the return value, stored in key "value".
+    """
+    container = {"value": 0}
+
+    try:
+        yield container
+
+    except KeyboardInterrupt:
+        container["value"] = 255
+        logger.info("Quit by user")
+
+    except DakaraError as error:
+        container["value"] = 1
+
+        # directly re-raise the error in debug mode
+        if debug:
+            raise
+
+        # just log it otherwise
+        logger.critical(error)
+
+    except BaseException as error:
+        container["value"] = 2
+
+        # directly re-raise the error in debug mode
+        if debug:
+            raise
+
+        # re-raise it and show a special message otherwise
+        logger.exception("Unexpected error: {}".format(error))
+        logger.critical("Please fill a bug report at '{}'".format(bugtracker_url))
--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,6 +1,12 @@
+import logging
 from unittest import TestCase
+from unittest.mock import ANY
 
-from dakara_base.exceptions import DakaraError, generate_exception_handler
+from dakara_base.exceptions import (
+    DakaraError,
+    generate_exception_handler,
+    handle_all_exceptions,
+)
 
 
 class GenerateExceptionHandleTestCase(TestCase):
@@ -11,3 +17,64 @@ class GenerateExceptionHandleTestCase(TestCase):
         with self.assertRaisesRegex(DakaraError, r"initial message\nhandler message"):
             with handler():
                 raise DakaraError("initial message")
+
+
+class HandleAllExceptionsTestCase(TestCase):
+    def test_normal_exit(self):
+        """Test a normal exit."""
+        logger = logging.getLogger("normal")
+        with handle_all_exceptions(logger, "url") as exit_value:
+            pass
+
+        self.assertEqual(exit_value["value"], 0)
+
+    def test_keyboard_interrupt(self):
+        """Test a Ctrl+C exit."""
+        logger = logging.getLogger("keyboard_interrupt")
+        with self.assertLogs("keyboard_interrupt") as log:
+            with handle_all_exceptions(logger, "url") as exit_value:
+                raise KeyboardInterrupt
+
+        self.assertEqual(exit_value["value"], 255)
+        self.assertListEqual(log.output, ["INFO:keyboard_interrupt:Quit by user"])
+
+    def test_known_error(self):
+        """Test a known error exit."""
+        logger = logging.getLogger("known_error")
+        with self.assertLogs("known_error") as log:
+            with handle_all_exceptions(logger, "url") as exit_value:
+                raise DakaraError("error")
+
+        self.assertEqual(exit_value["value"], 1)
+        self.assertListEqual(log.output, ["CRITICAL:known_error:error"])
+
+    def test_known_error_debug(self):
+        """Test a known error exit in debug mode."""
+        logger = logging.getLogger("known_error")
+        with self.assertRaisesRegex(DakaraError, "error"):
+            with handle_all_exceptions(logger, "url", True) as exit_value:
+                raise DakaraError("error")
+
+        self.assertEqual(exit_value["value"], 1)
+
+    def test_unknown_error(self):
+        """Test an unknown error exit."""
+        logger = logging.getLogger("unknown_error")
+        with self.assertLogs("unknown_error") as log:
+            with handle_all_exceptions(logger, "url") as exit_value:
+                raise Exception("error")
+
+        self.assertEqual(exit_value["value"], 2)
+        self.assertListEqual(
+            log.output,
+            [ANY, "CRITICAL:unknown_error:Please fill a bug report at 'url'"],
+        )
+
+    def test_unknown_error_debug(self):
+        """Test an unknown error exit in debug mode."""
+        logger = logging.getLogger("known_error")
+        with self.assertRaisesRegex(Exception, "error"):
+            with handle_all_exceptions(logger, "url", True) as exit_value:
+                raise Exception("error")
+
+        self.assertEqual(exit_value["value"], 2)
