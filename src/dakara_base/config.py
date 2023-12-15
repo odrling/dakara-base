6f86f0ee7abd51906cbc174eb66e039cc7a65708
odrling <florianbadie@odrling.xyz>
2023-12-15 23:07:12 +0100

    add strtobool function in utils
    Python 3.12 removed distutils and this utility function is not included
    in the setuptools package.
    PEP 632[1] suggests that strtobool should be reimplemented rather than
    relying on a compatibility package (unlike the rest of distutils).
    
    This reimplementation doesn't accept all the inputs that distutils used
    to accept but it is should work fine for our usage.
    
    [1]: https://peps.python.org/pep-0632/

--- src/dakara_base/config.py
+++ src/dakara_base/config.py
@@ -35,13 +35,14 @@ the configuration directory:
 
 import logging
 from collections import UserDict
-from distutils.util import strtobool
 
 import coloredlogs
 import progressbar
 import yaml
 from environs import Env, EnvError
 from path import Path
+
+from .utils import strtobool
 
 try:
     from importlib.resources import path
@@ -314,13 +315,9 @@ def create_config_file(resource, filename, force=False):
 
         # check destination does not exist
         if not force and destination.exists():
-            try:
-                result = strtobool(
-                    input("{} already exists, overwrite? [y/N] ".format(destination))
-                )
-
-            except ValueError:
-                result = False
+            result = strtobool(
+                input("{} already exists, overwrite? [y/N] ".format(destination))
+            )
 
             if not result:
                 return
