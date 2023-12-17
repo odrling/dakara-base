d94b65e51bff0f0d73b8a477c2eea25e4ab01001
odrling <florianbadie@odrling.xyz>
2023-12-17 12:14:55 +0100

    directory: fix AppDirsPath typing
    path.Path worked because it inherits from `str`. pathlib.Path can't be
    made to work in the same way so instead we can use composition.

--- src/dakara_base/directory.py
+++ src/dakara_base/directory.py
@@ -7,58 +7,61 @@ This module gives application name and project name:
 >>> PROJECT_NAME
 ... "DakaraProject"
 
-It also gives an evolved version of `appdirs.AppDirs` that returns `path.Path`
+It also gives an evolved version of `appdirs.AppDirs` that returns `pathlib.Path`
 objects:
 
 >>> type(directories.user_config_dir)
-... path.Path
+... pathlib.Path
 """
 from pathlib import Path
 
-from platformdirs import PlatformDirs as AppDirs
+from platformdirs import PlatformDirs, PlatformDirsABC
 
 APP_NAME = "dakara"
 PROJECT_NAME = "DakaraProject"
 
 
-class AppDirsPath(AppDirs):
-    """AppDirs class that returns `path.Path` objects."""
+class AppDirsPath:
+    """AppDirs class that returns `pathlib.Path` objects."""
+
+    def __init__(self, appdirs: PlatformDirsABC):
+        self.appdirs = appdirs
 
     @property
     def site_config_dir(self):
-        return Path(super().site_config_dir)
+        return Path(self.appdirs.site_config_dir)
 
     @property
     def site_data_dir(self):
-        return Path(super().site_data_dir)
+        return Path(self.appdirs.site_data_dir)
 
     @property
     def user_cache_dir(self):
-        return Path(super().user_cache_dir)
+        return Path(self.appdirs.user_cache_dir)
 
     @property
     def user_config_dir(self):
-        return Path(super().user_config_dir)
+        return Path(self.appdirs.user_config_dir)
 
     @property
     def user_data_dir(self):
-        return Path(super().user_data_dir)
+        return Path(self.appdirs.user_data_dir)
 
     @property
     def user_documents_dir(self):
-        return Path(super().user_documents_dir)
+        return Path(self.appdirs.user_documents_dir)
 
     @property
     def user_log_dir(self):
-        return Path(super().user_log_dir)
+        return Path(self.appdirs.user_log_dir)
 
     @property
     def user_runtime_dir(self):
-        return Path(super().user_runtime_dir)
+        return Path(self.appdirs.user_runtime_dir)
 
     @property
     def user_state_dir(self):
-        return Path(super().user_state_dir)
+        return Path(self.appdirs.user_state_dir)
 
 
-directories = AppDirsPath(APP_NAME, PROJECT_NAME, roaming=True)
+directories = AppDirsPath(PlatformDirs(APP_NAME, PROJECT_NAME, roaming=True))
--- tests/test_directory.py
+++ tests/test_directory.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 from unittest import TestCase
 
+from platformdirs import PlatformDirs
+
 from dakara_base.directory import AppDirsPath
 
 
 class AppDirsPathTestCase(TestCase):
     def test_properties(self):
-        appdirs = AppDirsPath("appname", "authorname")
+        appdirs = AppDirsPath(PlatformDirs("appname", "authorname"))
 
         self.assertIsInstance(appdirs.site_config_dir, Path)
         self.assertIsInstance(appdirs.site_data_dir, Path)
